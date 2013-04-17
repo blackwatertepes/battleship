155c4af998fa1896cbab9fdadb81ee67cf2e2bf7
Apprentice <apprentice@devbootcamp.com>
2013-04-16 20:16:42 -0700

    fixed bugs with all spaces now being units

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,11 +1,14 @@
 module PagesHelper
   def boat_classes(boat)
-    classes = [boat.ship.name.split(' ').first, boat.dir]
-    if boat.ship.length > 1
-      classes << "front" if boat.n == 0
-      classes << "back" if boat.n == boat.ship.length - 1
-    else
-      classes << "tiny"
+    classes = []
+    if boat && boat.ship
+      classes = [boat.ship.name.split(' ').first, boat.dir]
+      if boat.ship.length > 1
+        classes << "front" if boat.n == 0
+        classes << "back" if boat.n == boat.ship.length - 1
+      else
+        classes << "tiny"
+      end
     end
     classes
   end
