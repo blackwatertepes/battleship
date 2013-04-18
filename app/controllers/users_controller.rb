163908764f87e66d326f6caf67412392f5046adc
Apprentice <apprentice@devbootcamp.com>
2013-04-17 23:43:55 -0700

    added user edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,19 @@ class UsersController < ApplicationController
 
     redirect_to play_path
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.attributes = params[:user]
+    
+    if user.save
+      redirect_to play_path, :notice => "Name/Email Saved!"
+    else
+      redirect_to edit_user_path, :notice => "There was an error. Try again."
+    end
+  end
 end
