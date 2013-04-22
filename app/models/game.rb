ec2f8d8c6f7974266e56f5241f6a925c03c948dc
Tyler Kuhn <tyler@wargoats.com>
2013-04-22 15:36:20 -0700

    added user validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -107,201 +107,201 @@ class Game < ActiveRecord::Base
   end
 end
 
-class Board < Array
-  attr_accessor :board_size
-
-  def initialize(size)
-    @board_size = size
-  end
-
-  def injured
-    floating.select{|ship| ship.injured? }
-  end
-
-  def ships
-    fleet = []
-    shiped_spaces.each do |space|
-      fleet << space.ship unless fleet.include?(space.ship)
-    end
-    fleet
-  end
-
-  def spaces_by_ship(ship)
-    shiped_spaces.select{|unit| unit.ship == ship }
-  end
-
-  def shiped_spaces
-    set = []
-    self.each_with_index do |row, row_i|
-      row.each_with_index do |cell, cell_i|
-        set << cell if cell.ship?
-      end
-    end
-    set
-  end
-
-  def hits
-    set = []
-    self.each_with_index do |row, row_i|
-      row.each_with_index do |cell, cell_i|
-        set << cell if cell.hit?
-      end
-    end
-    set
-  end
-
-  def positive_targets
-    set = []
-    self.each_with_index do |row, row_i|
-      row.each_with_index do |cell, cell_i|
-        set << cell if cell.ship? && !cell.hit?
-      end
-    end
-    set
-  end
-
-  def negative_targets
-    set = []
-    self.each_with_index do |row, row_i|
-      row.each_with_index do |cell, cell_i|
-        set << cell unless cell.ship? || cell.hit?
-      end
-    end
-    set
-  end
-
-  def neighbors(ship)
-    set = []
-    if ship.hits > 1
-      spaces_by_ship(ship).each do |cell|
-        if cell.hit?
-          if cell.dir == 'right'
-            if cell.cell < @board_size - 1
-              target = self[cell.row][cell.cell + 1]
-              set << target if !target.hit?
-            end
-            if cell.cell > 0
-              target = self[cell.row][cell.cell - 1]
-              set << target if !target.hit?
-            end
-          else
-            if cell.row < @board_size - 1
-              target = self[cell.row + 1][cell.cell]
-              set << target if !target.hit?
-            end
-            if cell.row > 0
-              target = self[cell.row - 1][cell.cell]
-              set << target if !target.hit?
-            end
-          end
-        end
-      end
-    else
-      spaces_by_ship(ship).each do |cell|
-        if cell.hit?
-          if cell.row < @board_size - 1
-            target = self[cell.row + 1][cell.cell]
-            set << target if !target.hit?
-          end
-          if cell.row > 0
-            target = self[cell.row - 1][cell.cell]
-            set << target if !target.hit?
-          end
-          if cell.cell < @board_size - 1
-            target = self[cell.row][cell.cell + 1]
-            set << target if !target.hit?
-          end
-          if cell.cell > 0
-            target = self[cell.row][cell.cell - 1]
-            set << target if !target.hit?
-          end
-        end
-      end
-    end
-    set
-  end
-
-  def targets
-    unless injured.empty?
-      return neighbors(injured.sample)
-    else
-      return unhit
-    end
-  end
-
-  def unhit
-    set = []
-    self.each_with_index do |row, row_i|
-      row.each_with_index do |cell, cell_i|
-        set << cell unless cell.hit?
-      end
-    end
-    set
-  end
-
-  def clear?
-    floating.length == 0
-  end
-
-  def floating
-    ships.reject{ |ship| ship.sunk? }
-  end
-
-  def sunk
-    ships.select{ |ship| ship.sunk? }
-  end
-end
-
-class Unit
-  attr_accessor :ship, :n, :dir, :cell, :row
-
-  def initialize(params = nil)
-    if params
-      @ship = params[:ship]
-      @n = params[:n]
-      @dir = params[:dir]
-      @cell = params[:cell]
-      @row = params[:row]
-    end
-  end
-
-  def hit!
-    if ship?
-      @ship.body[@n] = 1
-    else
-      @ship = 1
-    end
-  end
-
-  def hit?
-    return @ship.body[@n] == 1 if ship?
-    return @ship == 1
-  end
-
-  def ship?
-    @ship.class == Ship
-  end
-end
-
-class Ship
-  attr_reader :name, :length
-  attr_accessor :body
-
-  def initialize(name, length)
-    @name = name
-    @length = length
-    @body = Array.new(length)
-  end
-
-  def injured?
-    hits > 0
-  end
-
-  def hits
-    @body.count(1)
-  end
-
-  def sunk?
-    hits == @length
-  end
-end
+# class Board < Array
+#   attr_accessor :board_size
+# 
+#   def initialize(size)
+#     @board_size = size
+#   end
+# 
+#   def injured
+#     floating.select{|ship| ship.injured? }
+#   end
+# 
+#   def ships
+#     fleet = []
+#     shiped_spaces.each do |space|
+#       fleet << space.ship unless fleet.include?(space.ship)
+#     end
+#     fleet
+#   end
+# 
+#   def spaces_by_ship(ship)
+#     shiped_spaces.select{|unit| unit.ship == ship }
+#   end
+# 
+#   def shiped_spaces
+#     set = []
+#     self.each_with_index do |row, row_i|
+#       row.each_with_index do |cell, cell_i|
+#         set << cell if cell.ship?
+#       end
+#     end
+#     set
+#   end
+# 
+#   def hits
+#     set = []
+#     self.each_with_index do |row, row_i|
+#       row.each_with_index do |cell, cell_i|
+#         set << cell if cell.hit?
+#       end
+#     end
+#     set
+#   end
+# 
+#   def positive_targets
+#     set = []
+#     self.each_with_index do |row, row_i|
+#       row.each_with_index do |cell, cell_i|
+#         set << cell if cell.ship? && !cell.hit?
+#       end
+#     end
+#     set
+#   end
+# 
+#   def negative_targets
+#     set = []
+#     self.each_with_index do |row, row_i|
+#       row.each_with_index do |cell, cell_i|
+#         set << cell unless cell.ship? || cell.hit?
+#       end
+#     end
+#     set
+#   end
+# 
+#   def neighbors(ship)
+#     set = []
+#     if ship.hits > 1
+#       spaces_by_ship(ship).each do |cell|
+#         if cell.hit?
+#           if cell.dir == 'right'
+#             if cell.cell < @board_size - 1
+#               target = self[cell.row][cell.cell + 1]
+#               set << target if !target.hit?
+#             end
+#             if cell.cell > 0
+#               target = self[cell.row][cell.cell - 1]
+#               set << target if !target.hit?
+#             end
+#           else
+#             if cell.row < @board_size - 1
+#               target = self[cell.row + 1][cell.cell]
+#               set << target if !target.hit?
+#             end
+#             if cell.row > 0
+#               target = self[cell.row - 1][cell.cell]
+#               set << target if !target.hit?
+#             end
+#           end
+#         end
+#       end
+#     else
+#       spaces_by_ship(ship).each do |cell|
+#         if cell.hit?
+#           if cell.row < @board_size - 1
+#             target = self[cell.row + 1][cell.cell]
+#             set << target if !target.hit?
+#           end
+#           if cell.row > 0
+#             target = self[cell.row - 1][cell.cell]
+#             set << target if !target.hit?
+#           end
+#           if cell.cell < @board_size - 1
+#             target = self[cell.row][cell.cell + 1]
+#             set << target if !target.hit?
+#           end
+#           if cell.cell > 0
+#             target = self[cell.row][cell.cell - 1]
+#             set << target if !target.hit?
+#           end
+#         end
+#       end
+#     end
+#     set
+#   end
+# 
+#   def targets
+#     unless injured.empty?
+#       return neighbors(injured.sample)
+#     else
+#       return unhit
+#     end
+#   end
+# 
+#   def unhit
+#     set = []
+#     self.each_with_index do |row, row_i|
+#       row.each_with_index do |cell, cell_i|
+#         set << cell unless cell.hit?
+#       end
+#     end
+#     set
+#   end
+# 
+#   def clear?
+#     floating.length == 0
+#   end
+# 
+#   def floating
+#     ships.reject{ |ship| ship.sunk? }
+#   end
+# 
+#   def sunk
+#     ships.select{ |ship| ship.sunk? }
+#   end
+# end
+# 
+# class Unit
+#   attr_accessor :ship, :n, :dir, :cell, :row
+# 
+#   def initialize(params = nil)
+#     if params
+#       @ship = params[:ship]
+#       @n = params[:n]
+#       @dir = params[:dir]
+#       @cell = params[:cell]
+#       @row = params[:row]
+#     end
+#   end
+# 
+#   def hit!
+#     if ship?
+#       @ship.body[@n] = 1
+#     else
+#       @ship = 1
+#     end
+#   end
+# 
+#   def hit?
+#     return @ship.body[@n] == 1 if ship?
+#     return @ship == 1
+#   end
+# 
+#   def ship?
+#     @ship.class == Ship
+#   end
+# end
+# 
+# class Ship
+#   attr_reader :name, :length
+#   attr_accessor :body
+# 
+#   def initialize(name, length)
+#     @name = name
+#     @length = length
+#     @body = Array.new(length)
+#   end
+# 
+#   def injured?
+#     hits > 0
+#   end
+# 
+#   def hits
+#     @body.count(1)
+#   end
+# 
+#   def sunk?
+#     hits == @length
+#   end
+# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,4 @@ class User < ActiveRecord::Base
   validates_presence_of :name, :email
   validates_uniqueness_of :email
   validates_format_of :email, with: /[\w\-\.]+@[\w\-\.]+\.[a-z]{2,5}/i
-
-  has_many :games
 end
