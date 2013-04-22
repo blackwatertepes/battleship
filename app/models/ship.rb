20e76f469196d61ad1f8d40d772925d33ea977c5
Tyler Kuhn <tyler@wargoats.com>
2013-04-22 16:03:11 -0700

    moved board and ship classes into their models

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,3 +1,15 @@
 class Ship < ActiveRecord::Base
   attr_accessible :board_id
+  
+  def injured?
+    # hits > 0
+  end
+
+  def hits
+    # @body.count(1)
+  end
+
+  def sunk?
+    # hits == @length
+  end
 end
